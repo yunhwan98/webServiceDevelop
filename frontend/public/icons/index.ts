7d7a2f062d5707975966353089b9019aa4a2cf26
박윤환(YunHwan Park) <yh107@coontec.com>
2023-12-12 21:09:10 +0900

    feat: logo 컴포넌트 및 icon 컴포넌트 생성

---
+++ frontend/public/icons/index.ts
@@ -0,0 +1,7 @@
+import { IoMdPerson as IconPerson } from "react-icons/io";
+import { FaShoppingCart as IconCart } from "react-icons/fa";
+import { IoLogOut as IconLogOut } from "react-icons/io5";
+import { FaGithub as IconGitHub } from "react-icons/fa";
+import { CiSearch as IconSearch } from "react-icons/ci";
+import { FaChevronDown as IconDown } from "react-icons/fa";
+export { IconPerson, IconCart, IconLogOut, IconGitHub, IconSearch, IconDown };
