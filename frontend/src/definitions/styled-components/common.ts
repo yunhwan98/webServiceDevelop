6433c1fc8a682bedc912360620c1d7c7b3de6658
박윤환(YunHwan Park) <yh107@coontec.com>
2023-12-12 21:02:22 +0900

    style: style 초기 설정

--- frontend/src/definitions/styled-components/common.ts
+++ frontend/src/definitions/styled-components/common.ts
@@ -3,12 +3,13 @@ import { BaseTheme } from "styled-components";
 // #endregion Global Imports
 
 const common: BaseTheme = {
-  colors: {
-    transparent: "transparent",
-    darkGrey: "#282C34",
-    blackGrey: "#20232A",
-    white: "#FFFFFF",
-  },
+    colors: {
+        transparent: "transparent",
+        darkGrey: "#282C34",
+        blackGrey: "#20232A",
+        grey: "#D9D9D9",
+        white: "#FFFFFF",
+    },
 };
 
 export { common };
