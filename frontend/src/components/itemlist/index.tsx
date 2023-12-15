d35fadef58f000fa097cf9ebaf77d3588c368b00
박윤환(YunHwan Park) <yh107@coontec.com>
2023-12-15 13:47:07 +0900

    feat: detail 페이지 라우팅 기능 구현

--- frontend/src/components/itemlist/index.tsx
+++ frontend/src/components/itemlist/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Container } from "./styled";
 import Image from "next/image";
 import { Item } from "@components/item";
+import Link from "next/link";
 
 export const ItemList: React.FC = () => {
     const arr = Array.from(Array(18), (_, i) => i + 1);
+
     return (
         <Container>
-            {arr.map((it) => (
-                <Item />
+            {arr.map((it, idx) => (
+                <Item id={String(idx)} />
             ))}
         </Container>
     );
